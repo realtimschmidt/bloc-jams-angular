1113c9cb30f5db380b2c7b440a07dc96275f2e2c
Timothy Schmidt <tim@Timothys-MacBook.local>
2017-03-19 22:17:13 -0400

    Update to SongPlayer.next method to complete Assignment 8 Services Part 3

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -126,7 +126,7 @@
             var currentSongIndex = getSongIndex(SongPlayer.currentSong);
             currentSongIndex++;
             
-            if (currentSongIndex > currentAlbum.songs.length) {
+            if (currentSongIndex > currentAlbum.songs.length - 1) {
                 stopSong(song);
             } else {
                 var song = currentAlbum.songs[currentSongIndex];
